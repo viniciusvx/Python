29b9ae59ab2f3e48d1e054fccbcf5f76f1136ccc
viniciusvx <vininavarro.2010@gmail.com>
2026-05-06 19:40:32 -0400

    atividade

--- Estruturas Condicionais/exercicio10.py
+++ Estruturas Condicionais/exercicio10.py
@@ -1,12 +1,17 @@
+a = int(input("Digite o valor de: a "))
+b = int(input("Digite o valor de: b "))
+c = int(input("Digite o valor de: c "))
 
-a=int(input("Digite o valor de: a"))
-b=int(input("Digite o valor de: b"))
-c=int(input("Digite o valor de: c"))
-
-delta= (b**2) - 4*a*c
-bask= (-b) +-
+delta = (b**2) - (4*a*c)
 
 print(f"O valor de delta é: {delta}")
 
-if delta<0:
-    print("Não exitem raizes reais")
+if delta < 0:
+    print("Não existem raízes reais")
+else:
+    x1 = (-b + (delta**0.5)) / (2*a)
+    x2 = (-b - (delta**0.5)) / (2*a)
+
+    print("Agora vou te mostrar Bhaskara")
+    print(f"Esse é x1 = {x1:.2f}")
+    print(f"Esse é x2 = {x2:.2f}")
---
+++ Estruturas Condicionais/exercicio11.py
@@ -0,0 +1,22 @@
+
+print("Voce está com febre?")
+res=int(input("1- sim 2- não\n"))
+if res== 1:
+ print("Voce está com nariz entupido?")
+res=int(input("1- sim 2- não\n"))
+if res== 1:
+ print("Voce está com dor de garganta?")
+res=int(input("1- sim 2- não\n"))
+if res== 1:
+ print("Voce está com tosse?")
+res=int(input("1- sim 2- não"))
+if res== 1:
+    print("GRIPADO!!")
+    exit()
+    
+else:
+    print("NÃO ESTÁ GRIPADO")
+    exit()
+
+
+ 
