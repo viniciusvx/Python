b4bba9cb49b6e8a95b12d537e8a3a1b05a2ae5d2
viniciusvx <vininavarro.2010@gmail.com>
2026-05-18 18:41:54 -0400

    v1

--- Estrutura Repetição/exercicio3.py
+++ Estrutura Repetição/exercicio3.py
@@ -1,114 +1,126 @@
-alunos = []
-disciplinas = []
+dic_alunos = {}
 
 while True:
+    
 
-    print("-" * 30)
-    print("=== MENU ESCOLAR ===")
+    print("=" * 30)
+    print("-----MENU ESCOLAR-----")
+    print("=" * 30)
 
-    opcao_menu = int(input(
-        "1 - CADASTRAR ALUNO\n"
-        "2 - CADASTRAR DISCIPLINA\n"
-        "3 - VER RELATÓRIO\n"
-        "4 - SAIR\n"
+    op = int(input(
+        "1-CADASTRAR ALUNO\n"
+        "2-NOTAS E DISCIPLINAS\n"
+        "3-BOLETIM\n"
+        "4-SAIR\n"
     ))
 
-    print("-" * 30)
+    # ================= CADASTRO =================
+    if op == 1:
 
-    if opcao_menu == 1:
+        print("===CADASTRO===")
+
+        nome = input("Informe o nome do aluno: ")
+        idade = int(input("Qual é a idade do aluno?: "))
+
+        if idade < 0:
+            print("Idade inválida!")
+            continue
+
+        turma_validas = ["1","2","3","4","5","6","7","8","9","1°","2°","3°"]
+
+        turma = input("1 a 9 (FUNDAMENTAL) - 1° a 3° (MÉDIO): ")
+
+        if turma not in turma_validas:# SE A TURMA QUE O USUARIO DIGITAR NÃO ESTIVER DENTRO DE TURMAS VALIDAS
+            print("Turma inválida!")
+            continue
+
+        genero = input("Qual é o genero do aluno:[F][M] ").upper()
+
+        if genero != "F" and genero != "M":# SE O GENERO DIGITADO FOR DIFERENTE DE(!=) F OU M
+            print("Gênero inválido!")
+            continue
+
+        dic_alunos[nome] = {   # AQUI ESTÁ SALVANDO AS INFORMAÇÕES [NOME DO ALUNO]
+            "idade": idade,    # IDADE : IDADE QUE O USUARIO DIGITOU
+            "turma": turma,    # TURMA: TURMA QUE O USUARIO DIGITOU
+            "genero": genero,  # GENERO: GENERO QUE O USUARIO DIGITOU
+            "disciplinas": {}  # DISCIPLINAS : ESTÁ VAZIA POIS QUANDO O USUARIO CADASTRAR VAI VIR PRA CÁ
+        }
+
+        print("Aluno cadastrado!!!")
+
+    # ================= NOTAS =================
+    elif op == 2:
+
+        print("===NOTAS & DISCIPLINAS===")
+
+        aluno_cadastrado = input("Qual aluno deseja cadastrar notas?: ")
+
+        if aluno_cadastrado not in dic_alunos: # SE O NOME DO ALUNO QUE ELE DIGITAR NÃO ESTIVER NO DICIONÁRIO
+            print("Aluno não encontrado!")
+            continue
 
         while True:
 
-            nome_aluno = input("Nome do aluno: ")
+            dis = input("Qual é a disciplina?: ")
+            notas = []
 
-            turmas_validas = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "1°", "2°", "3°"]
-
-            turma_aluno = input("Turma do aluno (1 a 9 ou 1°, 2°, 3°): ")
-
-            if turma_aluno not in turmas_validas: # verifica se a turma digitada pelo aluno está na lista de turmas válidas; se não estiver, considera inválida
-             print("Turma inválida!")
-              # interrompe a iteração atual do loop e volta para o início, pedindo novamente os dados ao usuário
-
-            idade_aluno = int(input("Idade do aluno: "))
-
-            if idade_aluno < 0:
-                print("Idade inválida!")
-                continue
-
-            sexo_aluno = input("Sexo [F/M]: ").upper()
-
-            if sexo_aluno != "F" and sexo_aluno != "M":
-                print("Sexo inválido!")
-                continue
-
-            aluno = {
-                "nome": nome_aluno,
-                "idade": idade_aluno,
-                "sexo": sexo_aluno,
-                "turma": turma_aluno,
-                "disciplinas": disciplinas.copy() # cria uma cópia da lista de disciplinas para este aluno, evitando que futuras alterações afetem alunos já cadastrados
-            }
-
-            alunos.append(aluno)
-
-            continuar = input("Cadastrar outro aluno? [S/N]: ").upper()
-
-            if continuar == "N":
-                break
-
-    elif opcao_menu == 2:
-
-        while True:
-
-            nome_disciplina = input("Nome da disciplina: ")
-
-            lista_notas_disciplina = []
-
-            for x in range(4):
-
+            for x in range(4): # SE EU COLOCASSE O WHILE EM CIMA, ELE NÃO IRIA PARAR O CODIGO QUANDO ESTIVESSE ERRADO
                 while True:
+                 nota = float(input(f"Digite a {x+1}° nota de {dis}: "))
+                 if nota<0 or nota >10:
+                    print("Nota inválida, digite novamente: ")
+                    continue
+                 else:
+                    notas.append(nota)
 
-                    valor_nota = float(input(f"Nota {x + 1} de {nome_disciplina}: "))
+            dic_alunos[aluno_cadastrado]["disciplinas"][dis] = notas
+            #DIC_ALUNOS[VINI][DISCIPLINA][MATEMATICA] = 10,9,8,7
 
-                    if valor_nota < 0 or valor_nota > 10:
-                        print("Nota inválida!")
-                    else:
-                        lista_notas_disciplina.append(valor_nota)
-                        break
+            op2 = input("Nova disciplina? [S][N] ").upper()
 
-            disciplinas.append({
-                "nome_disciplina": nome_disciplina,# armazena o nome da disciplina
-                  "notas_disciplina": lista_notas_disciplina  # armazena a lista de 4 notas dessa disciplina
-                  })  # adiciona essa disciplina (nome + notas) à lista de disciplinas do aluno
-    
-            
-
-            continuar = input("Cadastrar outra disciplina? [S/N]: ").upper()
-
-            if continuar == "N":
+            if op2 == "N":
                 break
 
-    elif opcao_menu == 3:
+    # ================= BOLETIM =================
+    elif op == 3:
 
-        print("\n===== RELATÓRIO DOS ALUNOS =====")
+        aluno = input("Qual aluno deseja ver?: ")
 
-        for aluno in alunos:
+        if aluno not in dic_alunos: # SE O ALUNO NÃO ESTIVER CADASTRADO NO DICIONÁRIO
+            print("Aluno não encontrado!")
+            continue
 
-            print("=" * 30)
-            print(f"Nome: {aluno['nome']}")
-            print(f"Idade: {aluno['idade']}anos")
-            print(f"Sexo: {aluno['sexo']}")
-            print(f"Turma: {aluno['turma']}")
+        print("=" * 30)
+        print(f"Aluno: {aluno}")
 
-            print("Disciplinas cadastradas:")
-            
-            for disciplina in disciplinas:  # percorre cada disciplina cadastrada na lista de disciplinas
-                print(f"- {disciplina['nome_disciplina']}: {disciplina['notas_disciplina']}")  # imprime o nome da disciplina e a lista de notas correspondentes
+        disciplinas = dic_alunos[aluno]["disciplinas"]
+        #Guarda as disciplinas do aluno numa variável chamada disciplinas
 
+        if not disciplinas:#SE NÃO TIVER NENHUMA DISCIPLINA CADASTRADA
+            print("Nenhuma disciplina cadastrada!")
+            continue
 
-    elif opcao_menu == 4:
-        print("Programa encerrado!")
+        for disciplina, notas in disciplinas.items():
+            #para cada matéria e suas notas, repita o código abaixo
+            media = sum(notas) / len(notas)
+
+            print("-" * 30)
+            print(f"Disciplina: {disciplina}")
+            print(f"Notas: {notas}")
+            print(f"Média: {media:.1f}")
+
+            if media >= 7:
+                print("APROVADO")
+            else:
+                print("REPROVADO")
+
+    # ================= SAIR =================
+    elif op == 4:
+
+        print("Saindo...")
         break
 
+    # ================= OPÇÃO INVÁLIDA =================
     else:
-        print("Opção inválida!") 
+        print("Opção inválida!")
