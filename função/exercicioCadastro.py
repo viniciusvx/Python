02237a46f5da127a5bd9137330bfab2f032be56e
viniciusvx <vininavarro.2010@gmail.com>
2026-05-19 21:50:06 -0400

    ATIVIDADE DEF

--- função/exercicioCadastro.py
+++ função/exercicioCadastro.py
@@ -1,12 +1,81 @@
 def cadastrar_aluno():
     aluno = {}
     
-    aluno ["nome"] = input("Digite o nome do aluno: ")
-    aluno ["idade"] = int(input("Digite a idade do aluno: "))
-    aluno ["sexo"] = input("Digite o nome do aluno: ")
-    aluno ["serie"] = int(input("Digite a serie do aluno: "))
+    
+    aluno["nome"] = input("Digite o nome do aluno: ")
+    aluno["idade"] = int(input("Digite a idade do aluno: "))
+    aluno["sexo"] = input("Digite o sexo do aluno: ")
+    aluno["serie"] = int(input("Digite a serie do aluno: "))
+    aluno["disciplinas"] = []  # CORREÇÃO: padronização do nome da chave
 
-def cadastrar_disciplina():
-    disciplina= {}
-    disciplina ["nome"]=input("Digite o nome da disciplina: ")
-    disciplina ["nota"]=float(input("Digite a nota da disciplina: "))
+    qtd_disciplina = int(input("Quantas disciplinas deseja cadastrar? "))
+    for i in range(qtd_disciplina):
+        disciplina = cadastrar_disciplina(i)
+        aluno["disciplinas"].append(disciplina)
+        
+    return aluno# SERVE PARA MOSTRAR O RESULTADO DA FUNÇAO ASSIM NÃO PRECISA DAR O PRINT
+
+
+def cadastrar_disciplina(i):
+    disciplina = {}
+    
+    disciplina["nome"] = input(f"Digite o nome da disciplina: {i + 1}° disciplina ")
+    disciplina["notas"] = []  # CORREÇÃO: antes estava "nota"
+
+    for i in range(4):  # CORREÇÃO: não sobrescrever variável i
+        nota = float(input(f"Digite a {i+1}° nota: "))  # CORREÇÃO da f-string
+        disciplina["notas"].append(nota)
+        
+          
+    disciplina["media"] = calcular_medida(disciplina["notas"])  # CORREÇÃO: média adicionada
+    
+    return disciplina # SERVE PARA MOSTRAR O RESULTADO DA FUNÇAO ASSIM NÃO PRECISA DAR O PRINT
+
+
+def calcular_medida(notas):
+    media = sum(notas) / len(notas) #SUM VAI SOMAR O NUMERO DE NOTAS E LEN VAI LER QAUNTAS NOTAS FORAM DIGITADAS
+    
+    return media # SERVE PARA MOSTRAR O RESULTADO DA FUNÇAO ASSIM NÃO PRECISA DAR O PRINT
+
+
+def menu():
+    alunos = []
+    while True:
+        print("____MENU____")
+        print("1- CADASTRAR ALUNO")
+        print("2- MOSTRAR RELATORIO")
+        print("3- SAIR")
+        op = input("Escolha um opção: ")
+
+        if op == "1":
+            aluno = cadastrar_aluno()
+            alunos.append(aluno)
+        
+        elif op == "2":
+            mostrar_relatorio(alunos)  # CORREÇÃO: faltava passar parâmetro
+            
+        
+        elif op == "3":
+            print("Saindo...")
+            break  # CORREÇÃO: sair do loop
+
+
+def mostrar_relatorio(alunos):
+    if len(alunos) == 0:
+        print("Nenhuma cadastro!")
+    else:
+        print("---RELATORIO---")
+        
+        for aluno in alunos:
+            print(f"NOME: {aluno['nome']}")
+            print(f"IDADE: {aluno['idade']}")
+            print(f"SEXO: {aluno['sexo']}")
+            print(f"SERIE: {aluno['serie']}")
+
+            print("\nDISCIPLINAS...")
+
+            for disciplina in aluno["disciplinas"]:
+                print(f"{disciplina['nome']} -> MEDIA: {disciplina['media']:.2f}")
+    
+
+menu()
---
+++ função/exercicioCadastroProduto.py
@@ -0,0 +1,92 @@
+'''CADASTRO DE PRODUTO    
+-CATEGORIA
+-QUANTIDADE DE ESTOQUE
+ PREÇO
+
+ RELATÓRIO
+ -CATEGORIA
+ QUANTIDADE
+ PREÇO
+ -SITUAÇÃO DE ESTOQUE
+ QUANTIDADE > 10 = BOM
+ QUANTIDADE >5 <10= MEDIO
+ QUANTIDADE <5 = RUIM
+    '''
+
+def cadastrar_produto():# PARA EU VER TUDO ISSO ABAIXO EU TENHI QUE CHAMAR O NOME DA FUNÇÃO
+    produto={} #PARA VER O DIC, TEM QUE CHAMAR O NOME DO DICIONÁRIO
+
+    produto["nome"] = input("Informe o nome do produto: ")
+    produto["preço"]= float(input("Informe o valor desse produto: "))
+    produto["categoria"] =input("Informe a categoria do produto: ")
+    produto["quantidade"] = int(input("Informe a quantidade do produto: "))
+
+    
+    
+    print("Cadastro realizado!!")
+    
+    return produto
+
+def menu():
+   produtos = []
+   while True:
+       print("--- MENU ---")
+       print("1 - CADASTRAR PRODUTO")
+       print("2 - VER RELATÓRIO")
+       print("3 - Sair")
+       op = input("Qual opção você deseja?: ")
+
+       if op == "1":
+          produto = cadastrar_produto()
+          produtos.append(produto)
+
+       elif op == "2":
+           mostrar_relatorio(produtos)
+
+       elif op == "3":
+           print("Saindo...")
+           exit()
+
+       else: 
+           print("opção inválida,digite novamente")
+           continue
+           
+           
+           
+
+def mostrar_relatorio(produtos):
+    if len(produtos) == 0:
+        print("Nenhum produto cadastrado!!")
+    else:
+        print("---RELATÓRIO---")
+
+        for produto in produtos:
+            print(f"NOME: {produto["nome"]}")
+            print(f"PREÇO: {produto["preço"]}R$")
+            print(f"CATEGORIA: {produto["categoria"]}")
+            print(f"QUANTIDADE: {produto["quantidade"]}")
+            if produto["quantidade"] >=10:
+                print ("a situação do estoque é boa")
+            elif produto["quantidade"] >=5:
+                print("A situação do estoque é média")
+            else:
+                print("A situação é péssima")
+            print("-"*30)
+
+
+menu()
+
+           
+           
+   
+
+
+
+       
+
+
+
+
+
+    
+    
